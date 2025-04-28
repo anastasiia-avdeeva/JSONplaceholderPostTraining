0bcf7b959f0ca355727c749cab1ce2292dc89e05
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-04-28 04:18:34 +0300

    finish code

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 const API_URL = "https://jsonplaceholder.typicode.com/posts";
 const ERROR_MSG = "Oops, something went wrong! Please, try again later.";
 
-const postsContainer = document.querySelector(".posts");
 const formElem = document.forms.newPost;
 const titleInput = formElem.elements.postTitle;
-// const titleInputError = document.getElementById("titleErr");
 const bodyInput = formElem.elements.postBody;
-// const bodyInputError = document.getElementById("bodyErr");
-const allInputErrors = document.querySelectorAll(".form__error-msg");
+
+const postsContainer = document.querySelector(".posts");
+const postsLst = document.querySelector(".posts__lst");
+const postsInfoElem = document.querySelector(".posts__info");
+const btn = document.querySelector(".form__btn");
 
 const formValidity = {
   title: false,
   body: false,
 };
 
-formElem.addEventListener("blur", processInputChange, true);
+formElem.addEventListener("blur", processInputBlur, true);
 
-function processInputChange(evt) {
+function processInputBlur(evt) {
   if (evt.target.tagName === "INPUT" || evt.target.tagName === "TEXTAREA") {
     const changedInput = evt.target;
     const changedInputGroup = evt.target.closest(".form__input-group");
     const errMsgElem = changedInputGroup.querySelector(".form__error-msg");
     toggleElemVisibility(errMsgElem, changedInput.value);
     updateFromValidity(changedInput, !!changedInput.value);
+    updateBtnState();
   }
 }
 
-function toggleElemVisibility(elem, visible) {
-  visible ? (elem.style.display = "none") : (elem.style.display = "block");
+function toggleElemVisibility(elem, hide) {
+  hide ? (elem.style.display = "none") : (elem.style.display = "block");
 }
 
 function updateFromValidity(elem, isValid) {
@@ -36,31 +38,101 @@ function updateFromValidity(elem, isValid) {
     : (formValidity.body = isValid);
 }
 
-formElem.addEventListener("submit", submitPost);
-
-function renderErrorMsg() {
-  const errorElem = createElemWithClass("p", "error-msg");
-  errorElem.textContent = ERROR_MSG;
-  postsContainer.append(errorElem);
+function updateBtnState() {
+  isFormValid()
+    ? btn.removeAttribute("disabled")
+    : btn.setAttribute("disabled", true);
 }
 
-function removeErrorMsg() {
-  const erMsgElem = document.querySelector(".error-msg");
-  if (erMsgElem) {
-    erMsgElem.remove();
+function isFormValid() {
+  return Object.values(formValidity).every(Boolean);
+}
+
+formElem.addEventListener("submit", handlePostSubmission);
+
+async function handlePostSubmission(evt) {
+  evt.preventDefault();
+  const submitted = await submitToServer();
+  renderPostsContent(submitted);
+  resetFormValidity();
+  formElem.reset();
+}
+
+async function submitToServer() {
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        title: titleInput.value,
+        body: bodyInput.value,
+        userId: 1,
+      }),
+    });
+
+    if (!response.ok) {
+      console.log("Cannot fetch data form the server");
+      throw new Error("HTTP Error: " + response.status);
+    }
+
+    const responseParsed = await response.json();
+    console.log(responseParsed);
+    return response.ok;
+  } catch (error) {
+    console.error("Error while loading from server", error);
+    return false;
   }
 }
 
-function submitPost(evt) {
-  evt.preventDefault();
+function renderPostsContent(success) {
+  hidePostsInfo();
+  if (success) {
+    removeErrorMsg();
+    postsLst.append(createPostElem());
+  } else {
+    renderErrorMsg();
+  }
+}
 
-  //   form.reset(); самое последнее
+function hidePostsInfo() {
+  toggleElemVisibility(postsInfoElem, true);
+}
+
+function createPostElem() {
+  const postElem = createElemWithClass("li", "post");
+  postElem.innerHTML = `<h2 class="post__title">${capitalizeFirstLetter(
+    titleInput.value
+  )}</h2>
+      <p class="post__body">${capitalizeFirstLetter(bodyInput.value)}</p>`;
+  return postElem;
 }
 
 function createElemWithClass(tagname, classname) {
   const newElem = document.createElement(tagname);
   newElem.classList.add(classname);
   return newElem;
+}
+
+function capitalizeFirstLetter(str) {
+  if (!str) return "";
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function renderErrorMsg() {
+  if (!document.querySelector(".posts__error-msg")) {
+    const errorElem = createElemWithClass("p", ".posts__error-msg");
+    errorElem.textContent = ERROR_MSG;
+    postsContainer.append(errorElem);
+  }
+}
+
+function removeErrorMsg() {
+  const erMsgElem = document.querySelector(".posts__error-msg");
+  if (erMsgElem) {
+    erMsgElem.remove();
+  }
 }
 
 function resetFormValidity() {
